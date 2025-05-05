dade7f91c9211d3de593786f2c6c2a2c772a9497
michgidev <michgi.dev@gmail.com>
2025-05-05 12:32:41 -0600

    feature/ Add useSearchParams to TodoSearch component

--- src/ui/TodoSearch/index.js
+++ src/ui/TodoSearch/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import './TodoSearch.css';
+import { useSearchParams } from 'react-router-dom';
 
-function TodoSearch({ searchValue, setSearchValue, loading }) {
+function TodoSearch({ setSearchValue, loading }) {
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const paramsValue = searchParams.get('search');
+
   const onSearchValueChange = (event) => {
     setSearchValue(event.target.value);
+    setSearchParams({ search: event.target.value})
   };
 
   return (
     <input
       className="TodoSearch"
       placeholder="Cebolla"
-      value={searchValue}
+      value={paramsValue ?? ''}
       onChange={onSearchValueChange}
       disabled={loading}
     />
